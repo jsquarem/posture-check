c52771ef6a1056f4cfc90b59f740ba327ba39738
jmartymar <jeffjmart@gmail.com>
2022-08-24 14:17:55 -0500

    cleanup code, add comments

--- posture_check.py
+++ posture_check.py
@@ -2,12 +2,9 @@ import cv2
 import mediapipe as mp
 import numpy as np
 from os import system
-# import pyttsx3
 
-# engine = pyttsx3.init()
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-
 
 cap = cv2.VideoCapture(0)
 # Setup MP instance
@@ -51,6 +48,7 @@ with mp_pose.Pose(
       x_values = [left_eye_x, right_eye_x, mouth_left_x, mouth_right_x]
       y_values = [left_eye_y, right_eye_y, mouth_left_y, mouth_right_y]
   
+      # Establish and update min and max values for the X and Y axes
       if i < 200 and i > 15:
         for value in x_values:
           if x_max < value or x_max == 0:
@@ -63,11 +61,17 @@ with mp_pose.Pose(
             y_max = value
           if y_min > value or y_min == 0:
             y_min = value  
-
       
     except:
       pass
 
+    # Build bountry box and text coordinates
+    rectangle_top_left = [x_min, y_max]
+    rectangle_bottom_right = [x_max, y_min]
+    x_middle = (x_max - x_min)/2 - (x_max - x_min)*.1 + x_min
+    calibrate_coords = [x_middle, y_max]
+
+    # Extablish and update Calibration text during calibration
     if i < 200 and i > 15:
       if i % 3 == 0:
         calibrate_text = 'Calibrating...'
@@ -78,6 +82,7 @@ with mp_pose.Pose(
     else:
       calibrate_text = 'Calibrated'
 
+    # Check current values against established boundries
     if i >= 200:
       for value in x_values:
         if x_max < value or x_min > value:
@@ -100,22 +105,14 @@ with mp_pose.Pose(
         count = 0
         print('Check Posture')
         system('say "Check posture"')
-
-
   
-    rectangle_top_left = [x_min, y_max]
-    rectangle_bottom_right = [x_max, y_min]
-    x_middle = (x_max - x_min)/2 - (x_max - x_min)*.1 + x_min
-    calibrate_coords = [x_middle, y_max]
-
-
+    # Draw boundry box and calibration text
     cv2.rectangle(image, tuple(np.multiply(rectangle_top_left, [1280, 720]).astype(int)), tuple(np.multiply(rectangle_bottom_right, [1280, 720]).astype(int)), (255,255,255), 1)
     cv2.putText(image, str(calibrate_text),
                   tuple(np.multiply(calibrate_coords, [1280, 720]).astype(int)),
                   cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2, cv2.LINE_AA
                 )
     
-
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS) # mp_pose.POSE_CONNECTIONS [mp_pose.PoseLandmark.RIGHT_EYE_OUTER, mp_pose.PoseLandmark.LEFT_EYE_OUTER]
 
     i += 1
